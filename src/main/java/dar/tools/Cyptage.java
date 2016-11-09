eff620f0c18f49e7ac4a8be7ab84dcf53fc2076b
naaf <serranashraf@gmail.com>
2016-11-09 20:55:06 +0100

    add fonction crypt

---
+++ src/main/java/dar/tools/Cyptage.java
@@ -0,0 +1,15 @@
+package dar.tools;
+
+import org.mindrot.jbcrypt.BCrypt;
+
+public class Cyptage {
+	private static String SALT = "$2a$10$tSBQ74s9Jzz1VxueEE3hFe";
+
+	public static String hashpw(String mdp) {
+		return BCrypt.hashpw(mdp, SALT);
+	}
+
+	public static boolean checkpw(String mdp, String hashed) {
+		return BCrypt.checkpw(mdp, hashed);
+	}
+}
