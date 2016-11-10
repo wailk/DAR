0898852512f136c419c9d2be59c73f966011f146
MOOTEZ <hichri.mootez@gmail.com>
2016-11-10 15:51:49 +0100

    rajout laltitude et longitude recherche par code postal

--- src/main/java/dar/servlet/AddAppServlet.java
+++ src/main/java/dar/servlet/AddAppServlet.java
@@ -25,72 +25,7 @@ public class AddAppServlet extends HttpServlet {
 
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String PAGE_NOT_LOGGED="/Login" ; 
-		response.setContentType("application/json");
-		response.setCharacterEncoding("UTF-8");
-		JSONObject json=new JSONObject() ; 
-
-		PrintWriter out=response.getWriter() ;
-		System.out.println("************");
-		HttpSession session=request.getSession() ; 
-
-		if(session.getAttribute("user")==null){
-
-			System.out.println("redirection vers la page d'authentification ...");
-			try{
-				json.put("c", 1);
-				json.put("d","user null");
-
-			}catch(Exception e1){
-				e1.printStackTrace();
-			}
-			out.print(json.toString());
-			System.out.println("Invalid shit ! ");
-			response.sendRedirect(PAGE_NOT_LOGGED);
-
-			return ; 
-		}
-
-		Users tmpUser=(Users)session.getAttribute("user") ;
-		int id_user = tmpUser.getId_user() ; 
-		Appartements app=new Appartements() ; 
-		try{
-			String adresse=request.getParameter("adresse") ; 
-			String description=request.getParameter("desc");
-			//int arron=Integer.parseInt(request.getParameter("arron")) ; 
-			String photo=request.getParameter("photo");
-			String superficie=request.getParameter("superficie") ;
-			String type=request.getParameter("type") ;
-			String meuble = request.getParameter("meuble");
-			Boolean m = Boolean.parseBoolean(meuble);
-			String chaine_loyer=request.getParameter("loyer");
-			double loyer = Double.parseDouble(chaine_loyer);
-			
-			AppartServices.addAppart(tmpUser, adresse, loyer, m);
-	
-		}catch(Exception e){
-			try{
-				json.put("c", 1);
-				json.put("d","Mauvais param");
-
-			}catch(Exception e1){
-				e.printStackTrace();
-			}
-			out.print(json.toString());
-			return ; 
-
-		}	
-
-		try{
-			json.put("c", 0);
-			json.put("d","ça marche");
-
-		}catch(Exception e){
-			e.printStackTrace();
-		}
-
-		out.print(json.toString());
-		return ; 
+		
 	}
 
 
--- src/main/java/dar/servlet/AptServlet.java
+++ src/main/java/dar/servlet/AptServlet.java
@@ -1,6 +1,7 @@
 package dar.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -8,20 +9,19 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.hibernate.SessionFactory;
 import org.json.JSONArray;
-
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+import org.json.JSONObject;
 
 import dar.core.Appartements;
+import dar.core.Users;
 import dar.dao.AppartementDao;
+import dar.services.AppartServices;
 import dar.utils.HandleHibernate;
 
-/**
- * Servlet implementation class RechercheServlet
- */
+
 @WebServlet("/apt")
 public class AptServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -46,8 +46,73 @@ public class AptServlet extends HttpServlet {
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		//response.setContentType("application/json");
-		//Appartements apt = new Appartements();
+		String PAGE_NOT_LOGGED="/Login" ; 
+		response.setContentType("application/json");
+		response.setCharacterEncoding("UTF-8");
+		JSONObject json=new JSONObject() ; 
+
+		PrintWriter out=response.getWriter() ;
+		System.out.println("************");
+		HttpSession session=request.getSession() ; 
+
+		if(session.getAttribute("user")==null){
+
+			System.out.println("redirection vers la page d'authentification ...");
+			try{
+				json.put("c", 1);
+				json.put("d","user null");
+
+			}catch(Exception e1){
+				e1.printStackTrace();
+			}
+			out.print(json.toString());
+			System.out.println("Invalid shit ! ");
+			response.sendRedirect(PAGE_NOT_LOGGED);
+
+			return ; 
+		}
+
+		Users tmpUser=(Users)session.getAttribute("user") ;
+		int id_user = tmpUser.getId_user() ; 
+		Appartements app=new Appartements() ; 
+		try{
+			String adresse=request.getParameter("adresse") ; 
+			String description=request.getParameter("desc");
+			//int arron=Integer.parseInt(request.getParameter("arron")) ; 
+			String photo=request.getParameter("photo");
+			String superficie=request.getParameter("superficie") ;
+			String type=request.getParameter("type") ;
+			String meuble = request.getParameter("meuble");
+			int code_postal = Integer.valueOf(request.getParameter("codepostal"));
+			Boolean m = Boolean.parseBoolean(meuble);
+			String chaine_loyer=request.getParameter("loyer");
+			double loyer = Double.parseDouble(chaine_loyer);
+			
+			AppartServices.addAppart(tmpUser, adresse, loyer, m,code_postal);
+	
+		}catch(Exception e){
+			try{
+				json.put("c", 1);
+				json.put("d","Mauvais param");
+
+			}catch(Exception e1){
+				e.printStackTrace();
+			}
+			out.print(json.toString());
+			return ; 
+
+		}	
+
+		try{
+			json.put("c", 0);
+			json.put("d","ça marche");
+
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+
+		out.print(json.toString());
+		return ; 
 	}
 
 }
